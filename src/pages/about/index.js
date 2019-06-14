2aee1791980bca777bc698a8d40d497233499b49
Will <rlagmlckd@gmail.com>
2019-06-14 16:05:04 +0900

    Fixing error newly organized SCSS directory

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Layout from '../../components/Layout';
 
-import '../../components/about.scss';
 import members from './members.json';
 
 export class About extends React.Component {
