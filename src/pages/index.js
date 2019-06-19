1ab1b2727d3d54ac044d828024ecacf64f455a7f
Will <rlagmlckd@gmail.com>
2019-06-19 15:48:47 +0900

    Discovered bug for landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,23 +5,23 @@ import logo from '../img/logo/logo-y.svg';
 import styled from 'styled-components';
 
 const LandingPage = styled.div`
+    background-color: $dark-blue;
     min-height: 100vh;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-
     font-family: 'Chicago', monotype;
 `
 
 const Logo = styled.img`
     width: 250px;
     margin-bottom: 10px;
-
 `
 
 const H1 = styled.h1`
     font-weight: bold;
+    font-family: 'Chicago', monotype;
 `
 
 const StyledLink = styled(Link)`
